72100f07e1434bc947c9e92490f0e0c8270c6ea3
bggd <gameovera@live.com>
2018-12-06 13:27:57 +0900

    Fix unreachable switch

--- src/gfx/gfx_texture.hpp
+++ src/gfx/gfx_texture.hpp
@@ -11,17 +11,17 @@ struct GfxTexture {
 
   void create_from_rgb(uint8_t* data, uint16_t width, uint16_t height)
   {
-    glGenTextures(1, &this->id);
-    glBindTexture(GL_TEXTURE_2D, this->id);
+    GL_CHECK(glGenTextures(1, &this->id));
+    GL_CHECK(glBindTexture(GL_TEXTURE_2D, this->id));
     DEBUG_ASSERT(glIsTexture(this->id) == GL_TRUE, assert_handler{});
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    GL_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST));
+    GL_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST));
+    GL_CHECK(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data));
   }
 
   void destroy()
   {
-    glDeleteTextures(1, &this->id);
+    GL_CHECK(glDeleteTextures(1, &this->id));
   }
 
 };
--- src/third_party/opengl.hpp
+++ src/third_party/opengl.hpp
@@ -89,6 +89,7 @@ bool load_gl() noexcept
 const char* opengl_err2str(GLenum err) noexcept
 {
   switch (err) {
+    case GL_NO_ERROR: return "GL_NO_ERROR";
     case GL_INVALID_ENUM: return "GL_INVALID_ENUM";
     case GL_INVALID_VALUE: return "GL_INVALID_VALUE";
     case GL_INVALID_OPERATION: return "GL_INVALID_OPERATION";
